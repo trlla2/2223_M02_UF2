878ce7573ff766997e834ab527a6422477d2d20f
trlla2 <116338442+trlla2@users.noreply.github.com>
2023-01-26 17:35:51 +0100

    asdf

---
+++ script.js
@@ -0,0 +1,8 @@
+db = db.getSiblingDB('abascal');
+
+db.Items.insert(
+	{"name": "'Poti'", "consumable": 1,"rarity": 100, "cost": 199, "key": 1,"rarity": 100, "weight":100, "usage":299, "fusionable": 0,
+                   "width":100, "height":10, "durability":100, "description":"asudf", "disassemble":false}
+
+db.weapoons.insert(
+	{"weapons":"epoada", "grip":100, "durability":1000, "range":100, "critical_rate":75, "recoil":100, "damage":100, "level":100,"cost":100, "description":"bombardeen peru"}
